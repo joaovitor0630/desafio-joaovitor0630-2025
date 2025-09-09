7cd85d56c15b07a600043d5f61d64fe1ff3cacf8
João Vitor <carvalhovito115@gmail.com>
2025-09-09 19:23:48 -0300

    01

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,6 +1,19 @@
+let animais = {
+  'REX': 'CÃO', 'BRINQUEDOSREX': ['RATO', 'BOLA'],
+  'MIMI': 'GATO', 'BRINQUEDOSMIMI': ['BOLA', 'LASER'],
+  'FOFO': 'GATO',  'BRINQUEDOSFOFO': ['BOLA','RATO', 'LASER'],
+  'ZERO': 'GATO', 'BRINQUEDOSZERO': ['RATO', 'BOLA'],
+  'BOLA': 'CÃO', 'BRINQUEDOSBOLA': ['CAIXA', 'NOVELO'],
+  'BEBE': 'CÃO', 'BRINQUEDOSBEBE': [ 'LASER', 'RATO', 'BOLA'],
+  'LOCO': 'JABUTI', 'BRINQUEDOSJABUTI': ['SKATE', 'RATO']
+}
+
+
+
 class AbrigoAnimais {
 
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
+    
   }
 }
 
