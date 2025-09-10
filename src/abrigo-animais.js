67ed9e018cd598fcc38ae9959d946d3d127c7545
João vitor <156925293+joaovitor0630@users.noreply.github.com>
2025-09-09 23:01:48 -0300

    Update abrigo-animais.js

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,4 +1,9 @@
-let animais = {
+
+let brinquedos = ['BOLA', 'LASER', 'CAIXA', 'NOVELO', 'SKATE'];
+
+let animais = ['REX', 'MIMI', 'FOFO', 'ZERO', 'BOLA', 'BEBE', 'LOCO'];
+
+let animal = {
   'REX': 'CÃO', 'BRINQUEDOSREX': ['RATO', 'BOLA'],
   'MIMI': 'GATO', 'BRINQUEDOSMIMI': ['BOLA', 'LASER'],
   'FOFO': 'GATO',  'BRINQUEDOSFOFO': ['BOLA','RATO', 'LASER'],
@@ -7,14 +12,60 @@ let animais = {
   'BEBE': 'CÃO', 'BRINQUEDOSBEBE': [ 'LASER', 'RATO', 'BOLA'],
   'LOCO': 'JABUTI', 'BRINQUEDOSJABUTI': ['SKATE', 'RATO']
 }
+ let invalido = false
+
+class validarAnimais{
+  constructor(dadosinseridos, dadosvalidos){
+    dadosinseridos.forEach(function(dado){
+      if (dadosvalidos.includes(dadosinseridos) == false){
+        return invalido = true
+        console.log('invalido');
+      }
+    });
+  }
+}
+
+
+  class adocao {
+    constructor (brinq, ani){
+      let resul = brinq.filter(function(item){
+        return ani.includes(item)
+      })
+      console.log(resul);
+    }
+  }
+
 
 
 
 class AbrigoAnimais {
 
+  
+
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
-    
-  }
+    new validarAnimais(brinquedosPessoa1.concat(brinquedosPessoa2), brinquedos);
+    new validarAnimais(ordemAnimais, animais);
+    new adocao(brinquedosPessoa1, ordemAnimais);
+    new adocao(brinquedosPessoa2, ordemAnimais);
+
+  };
 }
+ 
+
+new AbrigoAnimais().encontraPessoas(['BOLA', 'LASER'], ['CAIXA'], ['REX', 'MIMI', 'FOFO', 'ZERO', 'BOLA', 'BEBE']);
 
 export { AbrigoAnimais as AbrigoAnimais };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
